c45dfe7f8144d506c2f6d724abc515313a0079cf
azeem0730 <137784747+azeem0730@users.noreply.github.com>
2024-03-20 17:13:47 +0530

    changes in add forms in blog and career

--- src/components/ResourceCenter/AdminPage/NewJob/index.js
+++ src/components/ResourceCenter/AdminPage/NewJob/index.js
@@ -1,68 +1,137 @@
 import {
-    Container,
-    CreatePostBtn,
-    Container1,
-    Title,
-    ChooseFile,
-    Button,
-    NoFile,
-    Container2,
-    /* H1Cont,
+  GlobalStyle,
+  Container,
+  CreatePostBtn,
+  Container1,
+  Title,
+  Summary,
+  ChooseFile,
+  Button,
+  NoFile,
+  Container2,
+  DropDown,
+  /* H1Cont,
     Content, */
-    LastButton,
-  } from "./styled";
-  import React, { useState } from "react";
-  import ReactQuill from "react-quill";
-  import "react-quill/dist/quill.snow.css";
-  import styled from "styled-components";
-  
-  
-  const StyledQuill = styled(ReactQuill)`
-    /* Add your custom styles here */
+  LastButton,
+} from "./styled";
+
+import { ThemeProvider } from "styled-components";
+import React, { useState, useRef } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import styled from "styled-components";
+
+const StyledQuill = styled(ReactQuill)`
+  /* Add your custom styles here */
+  background-color: #d9d9d9;
+  border-radius: 5px;
+  border: 0px solid transparent;
+  outline: none;
+  .ql-editor {
+    font-size: 16px;
+    line-height: 1.5;
+    border: none;
+    color: #fff;
+    border-radius: 2px;
     background-color: #d9d9d9;
-    border-radius: 8px;
-    border: 0px solid transparent;
-    .ql-editor {
-      font-size: 16px;
-      line-height: 1.5;
-      border: 0px solid transparent;
-      border: none;
-      border-radius: 8px;
-      height: 200px; /* Adjust height as needed */
-      overflow-y: auto;
-    }
-  `;
-  
-  const modules = {
-    toolbar: [
-      [{ header: [1, 2, 3, 4, 5, 6, false] }],
-      [{ font: [] }],
-  
-      ["bold", "italic", "underline", "strike", "blockquote"],
-      [
-        { list: "ordered" },
-        { list: "bullet" },
-        { indent: "-1" },
-        { indent: "+1" },
-      ],
-      ["link", "image", "video"],
+    height: 200px; /* Adjust height as needed */
+    padding: 10px;
+    overflow-y: auto;
+    outline: none;
+  }
+  .ql-container {
+    border: none;
+  }
+`;
+
+const theme = {};
+const modules = {
+  toolbar: [
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+    [{ font: [] }],
+
+    ["bold", "italic", "underline", "strike", "blockquote"],
+    [
+      { list: "ordered" },
+      { list: "bullet" },
+      { indent: "-1" },
+      { indent: "+1" },
     ],
+    ["link", "image", "video"],
+  ],
+};
+
+const CreateBlog = () => {
+  const [value, setValue] = useState("");
+  const [selectedFile, setSelectedFile] = useState(null);
+  const fileInputRef = useRef(null);
+
+  const handleFileSelect = () => {
+    fileInputRef.current.click(); // Trigger file input when "Choose File" button is clicked
   };
-  
-  const CreateJob = () => {
-    const [value, setValue] = useState("");
-    return (
+
+  const handleFileUpload = (event) => {
+    const file = event.target.files[0];
+    setSelectedFile(file);
+    console.log("Selected file:", file);
+  };
+
+  const options = [
+    { value: 'option1', label: 'Select work mode' },
+    { value: 'option2', label: 'Hybrid' },
+    { value: 'option3', label: 'On site' },
+    { value: 'option4', label: 'Work From Home' },
+
+
+
+  ];
+  const [selectedOption, setSelectedOption] = useState(options[0].value);
+
+  const handleSelectChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
+  const selectStyle = {
+
+    backgroundColor: '#d9d9d9',
+    color: 'black',
+    padding: '8px',
+    borderRadius: '10px',
+    width: '200px',
+
+  };
+  const optionStyle = {
+    backgroundColor: '#d9d9d9',
+    color: 'black',
+
+  };
+
+
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <Container>
-        <CreatePostBtn>Create New Job Opportunity</CreatePostBtn>
-        <Container1>
-          <Title type="text" placeholder="Title" />
-          <Title type="text" placeholder="Experience" />
-          <Title type="text" placeholder="Salary Range" />
-          <ChooseFile>
-            <Button>Choose File</Button>
-            <NoFile>No file Chosen</NoFile>
-          </ChooseFile>
-          <Container2>
+      <Container1>
+        <Title type="text" placeholder="Title" />
+        <Title type="text" placeholder="Experience" />
+        <Title type="text" placeholder="Salary Range" />
+        <br></br><br></br>
+        <DropDown>
+          <div>
+            {/* <label htmlFor="dropdown">Select an option:</label> */}
+            <select style={selectStyle} id="dropdown" value={selectedOption} onChange={handleSelectChange}>
+              {options.map((option) => (
+                <option style={optionStyle} value={option.value}>
+                  {option.label}
+                </option>
+              ))}
+            </select>
+          </div>
+        </DropDown>
+        <ChooseFile>
+          <Button>Choose File</Button>
+
+        </ChooseFile>
+        <Container2>
           <StyledQuill
             theme="snow"
             value={value}
@@ -71,10 +140,10 @@ import {
             placeholder="Job Opportunity"
           />
         </Container2>
-          <LastButton>Create Job Opportunity</LastButton>
-        </Container1>
+        <LastButton>Create Job Opportunity</LastButton>
+      </Container1>
       </Container>
-    );
-  };
-  export default CreateJob;
-  
+    </ThemeProvider>
+  );
+};
+export default CreateBlog;
--- src/components/ResourceCenter/AdminPage/NewJob/styled.js
+++ src/components/ResourceCenter/AdminPage/NewJob/styled.js
@@ -1,14 +1,20 @@
 import styled from "styled-components";
+import { FaLocationDot } from "react-icons/fa6";
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle` 
+ *{ box-sizing: border-box;
+  margin: 0px;
+  font-family: "Inter";
+ }
+`;
 
 export const Container = styled.div`
-  width: 101%;
-  margin:-8px;
-
-  background-color: #0c111f;
+  width: 75%;
+  background-color: transparent;
   display: flex;
   flex-direction: column;
   justify-content: center;
-  padding: 80px 0 80px 0;
   align-items: center;
 `;
 
@@ -16,30 +22,29 @@ export const CreatePostBtn = styled.button`
   background-color: #435aa7;
   color: white;
   align-self: flex-end;
-  width: 250px;
-  height: 40px;
   font-size: 16px;
   margin-bottom: 20px;
   border: none;
-  margin: 0 140px 30px 0;
+  margin: 0 5% 30px 0;
   border-radius: 7px;
   text-align: center;
+  padding: 10px 20px;
 `;
 
 export const Container1 = styled.div`
-  width: 70%;
+  width: 95%;
   display: flex;
   flex-direction: column;
   justify-content: center;
   border-radius: 33px;
   border: 1px solid white;
-  margin-left: 50px;
+
   padding: 30px;
 `;
 
 export const Title = styled.input`
-  height: 60px;
-  width: 90%;
+  height: 40px;
+  width: 95%;
   border-radius: 10px;
   margin-left: 35px;
   margin-top: 20px;
@@ -47,6 +52,9 @@ export const Title = styled.input`
   padding-left: 30px;
   color: #263238;
   background-color: #d9d9d9;
+  outline: none;
+  border: none;
+  font-size: 13px;
 `;
 
 export const Summary = styled.input`
@@ -58,14 +66,16 @@ export const Summary = styled.input`
   color: #263238;
   padding-left: 30px;
   background-color: #d9d9d9;
+  outline: none;
+  font-size: 20px;
 `;
 
 export const ChooseFile = styled.div`
   height: 65px;
-  width: 90%;
+  width: 95%;
   border-radius: 10px;
   margin-left: 35px;
-  margin-top: 40px;
+  margin-top: 10px;
   background-color: #d9d9d9;
   padding-left: 30px;
   display: flex;
@@ -76,12 +86,13 @@ export const ChooseFile = styled.div`
 export const Button = styled.button`
   width: 90px;
   height: 30px;
-  background-color: #435aa7;
-  color: white;
+  background-color: #d9d9d9;
+  color: black;
   font-family: Sans-serif;
   font-size: 13px;
-  border: none;
+  border: 1px solid black;
 `;
+
 
 export const NoFile = styled.p`
   font-family: Sans-serif;
@@ -91,14 +102,24 @@ export const NoFile = styled.p`
 `;
 
 export const Container2 = styled.div`
-  height: 100px;
-  width: 90%;
-  border: 0px solid transparent;
-  margin-top: 30px;
+  height: 250px;
+  width: 95%;
+  border: 1px solid #2b459b; /* Border color */
+  margin-top: 15px;
   border-radius: 3px;
   margin-left: 30px;
+  overflow: hidden; /* Ensures children don't exceed container */
+  background-color: #d9d9d9; /* Remove background color */
+  color: white;
 `;
 
+export const DropDown = styled.div`
+margin-left:35px;
+margin-bottom:209px;
+  position:absolute;
+  font-family: Sans-serif;
+ 
+`;
 export const H1Cont = styled.div`
   height: 35px;
   width: 1000px;
@@ -117,13 +138,13 @@ export const Content = styled.p`
 
 export const LastButton = styled.button`
   height: 40px;
-  width: 90%;
+  width: 95%;
   border-radius: 10px;
   margin-left: 35px;
-  margin-top: 40px;
+  margin-top: 15px;
   color: #2b459b;
   font-weight: bold;
   padding-left: 30px;
   background-color: #d9d9d9;
-  margin-top:27%;
+  font-size: 20px;
 `;
