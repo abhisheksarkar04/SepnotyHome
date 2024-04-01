e1f6596bac08c71f660d52b82545de55641f105a
Yaminireddy-27 <yaminireddysavva@gmail.com>
2024-04-01 15:45:24 +0530

    changes

--- src/components/Testimonials/Slider/styledComponents.js
+++ src/components/Testimonials/Slider/styledComponents.js
@@ -1,12 +1,11 @@
 import Styled from "styled-components";
 
-
-
+import { Swiper, SwiperSlide } from "swiper/react";
 
 export const Main = Styled.div`
 position:on;
 background: linear-gradient(to left, #333, #333 55%, #eee 100%, #333 75%);
-`
+`;
 const StyledSwiper = Styled(Swiper)`
     @media screen and (max-width: 576px) {
         .swiper-slide {
@@ -14,8 +13,6 @@ const StyledSwiper = Styled(Swiper)`
         }
     }
 `;
-
-
 
 export const MainContainer = Styled.div`
 display: flex;
@@ -29,7 +26,7 @@ width:450px;
     height:200px;
     margin-left:-10px;
   }
-`
+`;
 export const CardContainer = Styled.div`
 background: #121415;
 border-radius:15px;
@@ -43,14 +40,14 @@ margin-bottom:10px;
     height: 250px;
     width: 400px;
   }
-`
+`;
 export const Stars = Styled.a`
 font-size:10px;
 margin-bottom:5px;
-`
+`;
 export const Para1 = Styled.p`
 color:#B7B6BD;
-font-family: Lexend;
+
 font-size: 18px;
 font-weight: 700;
 line-height: 33px;
@@ -65,10 +62,10 @@ margin-top:10px;
 @media screen and (max-width:555px){
     font-size: 8px;
   }
-`
+`;
 export const Para2 = Styled.p`
 color:#B7B6BD;
-font-family: Lexend;
+
 font-size: 18px;
 font-weight: 400;
 line-height: 38px;
@@ -84,29 +81,28 @@ margin-top:10px;
     font-size: 8px;
     line-height: 18px;
   }
-`
+`;
 export const ProfileCon = Styled.div`
 display: flex;
 flex-direction:row;
 gap:20px;
 margin-top:20px;
-`
+`;
 export const Image = Styled.img`
 @media screen and (max-width:555px){
     height: 20px;
     width:20px;
   }
 
-`
+`;
 export const Details = Styled.div`
 display: flex;
 flex-direction:column;
 
-`
+`;
 export const Name = Styled.a`
 color:#2B459B;
 font-size:15px;
-font-family: Lexend;
 
 
 @media screen and (max-width:455px){
@@ -118,11 +114,11 @@ font-family: Lexend;
 
     font-size:10px;
   }
-`
+`;
 export const Role = Styled.a`
 color:#2B459B;
 font-size:12px;
-font-family: Lexend;
+
 
 @media screen and (max-width:455px){
 
@@ -133,4 +129,4 @@ font-family: Lexend;
 
     font-size:8px;
   }
-`
+`;
